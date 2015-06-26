69c2b93fa1409c99abfe66c77e6b9d45445beb16
yurykiryla <yurykiryla@gmail.com>
2015-06-26 14:21:01 +0300

    0.0.1

---
+++ book/src/main/java/by/tranning/book/chain2/Example2.java
@@ -0,0 +1,17 @@
+package by.tranning.book.chain2;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		int num;
+		
+		num = 100;
+		
+		System.out.println("It's num variable: " + num);
+		
+		num = num * 2;
+		
+		System.out.println("num * 2 value is " + num);
+	}
+
+}
