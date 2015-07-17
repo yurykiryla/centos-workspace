ec746a9da1fc4b9fe92bb0cd81fd9e72cf2462e2
yurykiryla <yurykiryla@gmail.com>
2015-07-17 17:45:17 +0300

    asdd

--- book/src/main/java/by/tranning/book/App.java
+++ book/src/main/java/by/tranning/book/App.java
@@ -1,13 +1,23 @@
 package by.tranning.book;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		String[] test = new String[5];
+		test[3] = "sdfsdf";
+
+		List<String> strings = Arrays.asList(test);
+		for (String s : strings) {
+			System.out.println(s);
+		}
+
+	}
 }
